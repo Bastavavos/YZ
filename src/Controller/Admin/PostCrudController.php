871b31eb07a0686f75e830b80ca7e86423d66ba3
bastavavos <bastien.pelletier@orange.fr>
2024-12-06 16:12:07 +0100

    add dashboard admin in path /admin

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\Post;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -16,19 +15,14 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-
+    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
-
-            AssociationField::new('author'),
-
-
-
         ];
     }
-
+    */
 }
